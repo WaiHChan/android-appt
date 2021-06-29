9a65f1286cc0871edfc8333a66bcbc8848fd682e
WaiHChan <chanwai@pdx.edu>
2021-06-29 16:19:01 -0700

    testing

--- apptbook/src/main/java/edu/pdx/cs410J/chanwai/Project1.java
+++ apptbook/src/main/java/edu/pdx/cs410J/chanwai/Project1.java
@@ -7,10 +7,10 @@ public class Project1 {
 
   public static void main(String[] args) {
     Appointment appointment = new Appointment();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
+    /*System.err.println("Missing command line arguments");
     for (String arg : args) {
       System.out.println(arg);
-    }
+    }*/
 
 
 

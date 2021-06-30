9879c87333b72e650580a889d88335b73ed20ba9
WaiHChan <chanwai@pdx.edu>
2021-06-30 16:32:08 -0700

    testing -print

--- apptbook/src/main/java/edu/pdx/cs410J/chanwai/Appointment.java
+++ apptbook/src/main/java/edu/pdx/cs410J/chanwai/Appointment.java
@@ -13,13 +13,13 @@ public class Appointment extends AbstractAppointment {
 
   @Override
   public String getBeginTimeString() {
-    /**throw new UnsupportedOperationException("This method is not implemented yet");*/
+
     return this.beginDate + " " + this.beginTime;
   }
 
   @Override
   public String getEndTimeString() {
-    /**throw new UnsupportedOperationException("This method is not implemented yet");*/
+
     return this.endDate + " " + this.endTime;
   }
 

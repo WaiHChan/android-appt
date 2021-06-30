8b44b2628df96e53024b3ad8f1d2c44cb9e7758d
WaiHChan <chanwai@pdx.edu>
2021-06-29 23:09:29 -0700

    added readme text

--- apptbook/src/main/java/edu/pdx/cs410J/chanwai/Project1.java
+++ apptbook/src/main/java/edu/pdx/cs410J/chanwai/Project1.java
@@ -1,4 +1,7 @@
 package edu.pdx.cs410J.chanwai;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * The main class for the CS410J appointment book Project
@@ -8,10 +11,27 @@ public class Project1 {
   public static void main(String[] args) {
     Appointment appointment = new Appointment();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
 
+    if("-README".equals(args[0])) {
+      try {
+        File myfile = new File("apptbook/src/main/resources/edu/pdx/cs410J/chanwai/README.txt");
+        Scanner myScanner = new Scanner(myfile);
+        while (myScanner.hasNextLine()) {
+          String txt = myScanner.nextLine();
+          System.out.println(txt);
+        }
+        myScanner.close();
+      } catch (FileNotFoundException exception) {
+        System.out.println("Can not find the file.");
+        exception.printStackTrace();
+      }
+
+    }
+    /**
     System.err.println("Missing command line arguments");
     for (String arg : args) {
       System.out.println(arg);
     }
+     */
 
     System.out.print(args[0]);
     System.out.print("\n");

2feadff9919daa87c4345f1d4873aff68997fa14
WaiHChan <chanwai@pdx.edu>
2021-07-06 18:11:59 -0700

    add koan

---
+++ apptbook/src/main/java/edu/pdx/cs410J/chanwai/TextDumper.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410J.chanwai;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import edu.pdx.cs410J.AppointmentBookDumper;
+
+import java.io.IOException;
+
+public class TextDumper implements AppointmentBookDumper {
+
+    @Override
+    public void dump(AbstractAppointmentBook book) throws IOException {
+
+    }
+}

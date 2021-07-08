4ddeae6cf98084affa462ed0d34c7543d63a6477
TheDragonMask <andrew.niven95@gmail.com>
2021-07-07 19:57:19 -0700

    Refactored code, one error was we were comparing char to int, rather than char to char, ie. '3' == 3 instead of '3' == '3'

--- FooBarQix/src/main/java/edu/pdx/cs410J/KataCartographers/Kata.java
+++ FooBarQix/src/main/java/edu/pdx/cs410J/KataCartographers/Kata.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs410J.KataCartographers;
 
+import static java.lang.Integer.parseInt;
+
 /**
  * A class for getting started with a code kata
  *
@@ -16,61 +18,37 @@ public class Kata {
   }
 
   public static String compute(String arg) {
+    int argAsInt = parseInt(arg);
     String result = "";
-    boolean active = false; //Matt's idea, not quite sure how it'll work yet
-    while(arg.length() != 0) {
-      //STAGE 1: Check for divisibility
-      //STAGE 1a: Check divisible by 3
-      if (arg.charAt(0) % 3 == 0){
-        active = true;
-        result = result.concat("Foo");
-        //STAGE 2a: Check if 3
-        if (arg.charAt(0) == 3){
-          result = result.concat("Foo");
-        }
-      }
-      //STAGE 1b: Check divisible by 5
-      if (arg.charAt(0) % 5 == 0){
-        active = true;
-        result = result.concat("Bar");
-        //STAGE 2b: Check if 5
-        if (arg.charAt(0) == 5){
-          result = result.concat("Bar");
-        }
-      }
-      //STAGE 1c: Check divisible by 7
-      if (arg.charAt(0) % 7 == 0){
-        active = true;
-        result = result.concat("Qix");
-        //STAGE 2c: Check if 7
-        if (arg.charAt(0) == 7){
-          result = result.concat("Qix");
-        }
-      }
-      if(active != true)
-      {
-          result = result.concat(arg);
-      }
-      arg = arg.substring(1);
-      //if(active)
-      //{
-      //  arg = arg.substring(1);
-      //}
-      //else
-      //{
-      //  arg = arg.substring(1);
-      //  result = result.concat(arg);
-      //}
+    //STAGE 1: Check for divisibility
+    //STAGE 1a: Check divisible by 3
+    if (argAsInt % 3 == 0) {
+      result = result.concat("Foo");
     }
-    //if(active)
-    //{
-    //  arg = arg.substring(1);
-    //}
-    //else
-    //{
-    //  arg = arg.substring(1);
-    //  result = result.concat(arg);
-    //}
+    //STAGE 1b: Check divisible by 5
+    if (argAsInt % 5 == 0) {
+      result = result.concat("Bar");
+    }
+    //STAGE 1c: Check divisible by 7
+    if (argAsInt % 7 == 0) {
+      result = result.concat("Qix");
+    }
+    //STAGE 2: Check each digit if 3, 5 or 7
+    while (arg.length() != 0) {
+      //STAGE 2a: Check if 3
+      if (arg.charAt(0) == '3') {
+        result = result.concat("Foo");
+      }
+      else if (arg.charAt(0) == '5') {
+        result = result.concat("Bar");
+      }
+      //STAGE 2c: Check if 7
+      else if (arg.charAt(0) == '7') {
+        result = result.concat("Qix");
+      }
+      //cutting down arg by one digit
+      arg = arg.substring(1);
+    }
     return result;
   }
 }

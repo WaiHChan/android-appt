5ac96be430dcbd7fe27aa352dea7b85f1dd0cdfa
TheDragonMask <andrew.niven95@gmail.com>
2021-07-07 19:05:43 -0700

    Added skeleton of String compute(String arg)

--- FooBarQix/src/main/java/edu/pdx/cs410J/KataCartographers/Kata.java
+++ FooBarQix/src/main/java/edu/pdx/cs410J/KataCartographers/Kata.java
@@ -10,7 +10,15 @@ public class Kata {
                                                                                     
 
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    //System.err.println("Missing command line arguments");
     System.exit(1);
+  }
+
+  public static String compute(String arg) {
+    String temp = "";
+    //STAGE 1: Check for divisibility
+    //STAGE 1a: Check divisible by 3
+    temp = temp.concat(arg);
+    return temp;
   }
 }

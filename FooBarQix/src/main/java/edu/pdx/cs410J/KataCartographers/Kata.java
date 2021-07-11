dcd6fbc429c82b57004f22396cc5f1c9fe931e50
TheDragonMask <andrew.niven95@gmail.com>
2021-07-11 15:42:22 -0700

    Refactored so that Step 2 functionality is met while keeping Step 1 working, added integration tests matching those at https://codingdojo.org/kata/FooBarQix/

--- FooBarQix/src/main/java/edu/pdx/cs410J/KataCartographers/Kata.java
+++ FooBarQix/src/main/java/edu/pdx/cs410J/KataCartographers/Kata.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs410J.KataCartographers;
 
+import static java.lang.Integer.bitCount;
 import static java.lang.Integer.parseInt;
 
 /**
@@ -23,53 +24,76 @@ public class Kata {
   }
 
   public static String compute(String arg) {
+    String originalArg = arg; //for if no match is found at end, STAGE 3
     int argAsInt = parseInt(arg);
     String result = "";
-    boolean foundMatch = false;
+    boolean foundDivisibleMatch = false;
+    boolean foundEqualsMatch = false;
+    boolean foundZero = false;
     //STAGE 1: Check for divisibility
     //STAGE 1a: Check divisible by 3
     if (argAsInt % 3 == 0) {
       result = result.concat("Foo");
-      foundMatch = true;
+      //foundEqualsMatch = true;
+      foundDivisibleMatch = true;
     }
     //STAGE 1b: Check divisible by 5
     if (argAsInt % 5 == 0) {
       result = result.concat("Bar");
-      foundMatch = true;
+      //foundEqualsMatch = true;
+      foundDivisibleMatch = true;
     }
     //STAGE 1c: Check divisible by 7
     if (argAsInt % 7 == 0) {
       result = result.concat("Qix");
-      foundMatch = true;
+      //foundEqualsMatch = true;
+      foundDivisibleMatch = true;
     }
     //STAGE 2: Check each digit if 3, 5 or 7
     while (arg.length() != 0) {
       //STAGE 2a: Check if 3
       if (arg.charAt(0) == '3') {
         result = result.concat("Foo");
-        foundMatch = true;
+        foundEqualsMatch = true;
       }
       //STAGE 2b: Check if 5
       else if (arg.charAt(0) == '5') {
         result = result.concat("Bar");
-        foundMatch = true;
+        foundEqualsMatch = true;
       }
       //STAGE 2c: Check if 7
       else if (arg.charAt(0) == '7') {
         result = result.concat("Qix");
-        foundMatch = true;
+        foundEqualsMatch = true;
       }
       //STAGE 2d: Check if 0
       else if (arg.charAt(0) == '0') {
         result = result.concat("*");
-        foundMatch = true;
+        foundEqualsMatch = true;
+        foundZero = true;
       }
-      //STAGE 2e: Check if anything ELSE and NO MATCH FOUND BEFORE
-      else if (foundMatch == false){
+      //STAGE 2e: check if anything else
+      //secondary stage for numbers like "101", which should be "1*1"
+      //but came out as "*" before
+      //ended up being pretty complicated, what with hading to add "foundDivisibleMatch"
+      //because of numbers like "10101" which came out as "FooQix*1" before I fixed it
+      else if ((foundDivisibleMatch == false) && (foundEqualsMatch == false || foundZero == true)){
         result = result.concat(String.valueOf(arg.charAt(0)));
+        //disabling these as a test
+        //foundDivisibleMatch = false;
+        //foundEqualsMatch = false; //keeping this false for numbers like "101" so we don't get "1*"
+        //foundZero = false; //resetting this as a test
       }
       //cutting down arg by one digit
       arg = arg.substring(1);
+      //foundDivisibleMatch = false; //resetting this after the first pass
+    }
+    //STAGE 3: Check if the digit is anything ELSE and NO MATCH WAS FOUND BEFORE
+    if(foundDivisibleMatch == false && foundEqualsMatch == false && foundZero == false) {
+      //caused a problem with numbers like "11" just outputting "1"
+      //result = result.concat(String.valueOf(originalArg.charAt(0)));
+      //we need the full string back if there are NO matches
+      result = originalArg;
     }
     return result;
   }

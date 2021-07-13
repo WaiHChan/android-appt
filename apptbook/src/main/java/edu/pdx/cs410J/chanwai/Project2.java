ca357c1fdd0bea1cd86ba3eac7a172819f379eff
WaiHChan <chanwai@pdx.edu>
2021-07-13 12:08:11 -0700

    fixing line 69

--- apptbook/src/main/java/edu/pdx/cs410J/chanwai/Project2.java
+++ apptbook/src/main/java/edu/pdx/cs410J/chanwai/Project2.java
@@ -66,7 +66,7 @@ public class Project2 {
                 endDate = isDateCorrect(arg);
             } else if (endTime == null){
                 endTime = isTimeCorrect(arg);
-            } else if (trash == null){
+            } else{// if (trash == null){
                 trash = arg;
             }
         }

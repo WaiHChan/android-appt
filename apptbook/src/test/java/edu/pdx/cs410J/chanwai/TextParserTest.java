c2dba7919509a55e0aa7208bfd0d47c2c4655b39
WaiHChan <chanwai@pdx.edu>
2021-07-18 01:56:36 -0700

    work on textparser

--- apptbook/src/test/java/edu/pdx/cs410J/chanwai/TextParserTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/chanwai/TextParserTest.java
@@ -3,6 +3,11 @@ package edu.pdx.cs410J.chanwai;
 import edu.pdx.cs410J.ParserException;
 import org.junit.jupiter.api.Test;
 import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,58 +21,54 @@ public class TextParserTest {
      * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
      * @throws ParserException Throw an Parser exception if the appointment book cannot be read
      */
-//    @Test
-//    void appointmentBookOwnerCanBeDumpedAndParsed() throws IOException, ParserException {
-//        String owner = "Owner";
-//        String description = "Eyes";
-//        String begin_Date = "1/2/1554";
-//        String begin_Time = "12:42";
-//        String end_Date = "1/2/2005";
-//        String end_Time = "12:42";
-//
-//        Appointment appt = new Appointment(owner, description, begin_Date, begin_Time, end_Date, end_Time);
-//        AppointmentBook book = new AppointmentBook(owner);
-//        book.addAppointment(appt);
-//
-//        StringWriter sw = new StringWriter();
-//        TextDumper dumper = new TextDumper(sw);
-//        dumper.dump(book);
-//
-//        TextParser parser = new TextParser(new StringReader(sw.toString()));
-//        book = parser.parse();
-//
-//        assertThat(book.getOwnerName(), equalTo(owner));
-//    }
-//
-//    /**
-//     * Tests that invoking the dump() and parse() method from TestDump class and TestParser class
-//     * Check if an appointment book is created and saved in StringWriter and
-//     * Check if an appointment book can be parsed after the dump
-//     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
-//     * @throws ParserException Throw an Parser exception if the appointment book cannot be read
-//     */
-//    @Test
-//    void appointmentBookParsed() throws IOException, ParserException {
-//        String owner = "\"Owner One\"";
-//        String description = "Eyes";
-//        String begin_Date = "1/2/1554";
-//        String begin_Time = "12:42";
-//        String end_Date = "1/2/2005";
-//        String end_Time = "12:42";
-//
-//        Appointment appt = new Appointment(owner, description, begin_Date, begin_Time, end_Date, end_Time);
-//        AppointmentBook book = new AppointmentBook(owner);
-//        book.addAppointment(appt);
-//
-//        StringWriter sw = new StringWriter();
-//        TextDumper dumper = new TextDumper(sw);
-//        dumper.dump(book);
-//
-//        TextParser parser = new TextParser(new StringReader(sw.toString()));
-//        book = parser.parse();
-//
-//        assertThat(book.getOwnerName(), equalTo(owner));
-//    }
+    @Test
+    void appointmentBookOwnerCanBeDumpedAndParsed() throws IOException, ParserException, ParseException {
+        String description = "Eyes Check";
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+        var app = new Appointment("Jim", description, begin_date, end_date);
+
+        AppointmentBook book = new AppointmentBook("Jim");
+        book.addAppointment(app);
+
+        StringWriter sw = new StringWriter();
+        TextDumper dumper = new TextDumper(sw);
+        dumper.dump(book);
+
+        TextParser parser = new TextParser(new StringReader(sw.toString()));
+        book = parser.parse();
+
+        assertThat(book.getOwnerName(), equalTo("Jim"));
+    }
+
+    /**
+     * Tests that invoking the dump() and parse() method from TestDump class and TestParser class
+     * Check if an appointment book is created and saved in StringWriter and
+     * Check if an appointment book can be parsed after the dump
+     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
+     * @throws ParserException Throw an Parser exception if the appointment book cannot be read
+     */
+    @Test
+    void appointmentBookParsed() throws IOException, ParserException, ParseException {
+        String owner = "Jim";
+        String description = "Eyes Check";
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+        var app = new Appointment(owner, description, begin_date, end_date);
+        AppointmentBook book = new AppointmentBook(owner);
+        book.addAppointment(app);
+
+        StringWriter sw = new StringWriter();
+        TextDumper dumper = new TextDumper(sw);
+        dumper.dump(book);
+
+        TextParser parser = new TextParser(new StringReader(sw.toString()));
+        book = parser.parse();
+
+        assertThat(book.getOwnerName(), equalTo(owner));
+    }
 //
 //    /**
 //     * Tests that invoking the dump() and parse() method
@@ -75,11 +76,17 @@ public class TextParserTest {
 //     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
 //     */
 //    @Test
-//    void missingOwner() throws IOException{
-//
-//        Appointment appt = new Appointment("", "", "", "", "", "");
+//    void missingOwner() throws IOException, ParseException {
+//        String owner = "Jim";
+//        String description = "Eyes Check";
+//        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+//        Date begin_date = df.parse("" + "" + "");
+//        //Date begin_date = new Date(0,0,0,0,0);
+//        //Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+//        Date end_date = new Date(0,0,0,0,0);
+//        var app = new Appointment("", "", begin_date, end_date);
 //        AppointmentBook book = new AppointmentBook();
-//        book.addAppointment(appt);
+//        book.addAppointment(app);
 //
 //        StringWriter sw = new StringWriter();
 //        TextDumper dumper = new TextDumper(sw);
@@ -97,11 +104,15 @@ public class TextParserTest {
 //     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
 //     */
 //    @Test
-//    void missingDescription() throws IOException{
-//
-//        Appointment appt = new Appointment("Jim", "", "", "", "", "");
+//    void missingDescription() throws IOException, ParseException {
+//        String owner = "Jim";
+//        String description = "Eyes Check";
+//        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+//        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+//        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+//        var app = new Appointment(owner, description, begin_date, end_date);
 //        AppointmentBook book = new AppointmentBook();
-//        book.addAppointment(appt);
+//        book.addAppointment(app);
 //
 //        StringWriter sw = new StringWriter();
 //        TextDumper dumper = new TextDumper(sw);
@@ -119,11 +130,15 @@ public class TextParserTest {
 //     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
 //     */
 //    @Test
-//    void missingBeginDate() throws IOException{
-//
-//        Appointment appt = new Appointment("Jim", "Temp", "", "", "", "");
+//    void missingBeginDate() throws IOException, ParseException {
+//        String owner = "Jim";
+//        String description = "Eyes Check";
+//        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+//        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+//        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+//        var app = new Appointment(owner, description, begin_date, end_date);
 //        AppointmentBook book = new AppointmentBook();
-//        book.addAppointment(appt);
+//        book.addAppointment(app);
 //
 //        StringWriter sw = new StringWriter();
 //        TextDumper dumper = new TextDumper(sw);
@@ -141,11 +156,15 @@ public class TextParserTest {
 //     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
 //     */
 //    @Test
-//    void missingBeginTime() throws IOException{
-//
-//        Appointment appt = new Appointment("Jim", "Temp", "12/2/1493", "", "", "");
+//    void missingBeginTime() throws IOException, ParseException {
+//        String owner = "Jim";
+//        String description = "Eyes Check";
+//        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+//        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+//        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+//        var app = new Appointment(owner, description, begin_date, end_date);
 //        AppointmentBook book = new AppointmentBook();
-//        book.addAppointment(appt);
+//        book.addAppointment(app);
 //
 //        StringWriter sw = new StringWriter();
 //        TextDumper dumper = new TextDumper(sw);
@@ -163,11 +182,15 @@ public class TextParserTest {
 //     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
 //     */
 //    @Test
-//    void missingEndDate() throws IOException{
-//
-//        Appointment appt = new Appointment("Jim", "Temp", "12/2/1493", "12:22", "", "");
+//    void missingEndDate() throws IOException, ParseException {
+//        String owner = "Jim";
+//        String description = "Eyes Check";
+//        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+//        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+//        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+//        var app = new Appointment(owner, description, begin_date, end_date);
 //        AppointmentBook book = new AppointmentBook();
-//        book.addAppointment(appt);
+//        book.addAppointment(app);
 //
 //        StringWriter sw = new StringWriter();
 //        TextDumper dumper = new TextDumper(sw);
@@ -185,11 +208,15 @@ public class TextParserTest {
 //     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
 //     */
 //    @Test
-//    void missingEndTime() throws IOException{
-//
-//        Appointment appt = new Appointment("Jim", "Temp", "12/2/1493", "12:22", "1/2/1995", "");
+//    void missingEndTime() throws IOException, ParseException {
+//        String owner = "Jim";
+//        String description = "Eyes Check";
+//        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+//        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+//        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+//        var app = new Appointment(owner, description, begin_date, end_date);
 //        AppointmentBook book = new AppointmentBook();
-//        book.addAppointment(appt);
+//        book.addAppointment(app);
 //
 //        StringWriter sw = new StringWriter();
 //        TextDumper dumper = new TextDumper(sw);

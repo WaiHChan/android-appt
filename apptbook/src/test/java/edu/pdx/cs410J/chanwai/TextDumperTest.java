470edc4a22345c4c678efec8c3757eb7c8602fa9
WaiHChan <chanwai@pdx.edu>
2021-07-18 01:21:55 -0700

    finished TextDumperTest

--- apptbook/src/test/java/edu/pdx/cs410J/chanwai/TextDumperTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/chanwai/TextDumperTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -14,26 +18,23 @@ public class TextDumperTest {
      * It shows that both methods run correctly
      * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
      */
-//    @Test
-//    void dumperDumpsAppointmentBookOwner() throws IOException {
-//        String owner = "Owner";
-//        String description = "Eyes";
-//        String begin_Date = "1/2/1554";
-//        String begin_Time = "12:42";
-//        String end_Date = "1/2/2005";
-//        String end_Time = "12:42";
-//
-//        Appointment appt = new Appointment(owner, description, begin_Date, begin_Time, end_Date, end_Time);
-//        AppointmentBook book = new AppointmentBook(owner);
-//        book.addAppointment(appt);
-//
-//        StringWriter sw = new StringWriter();
-//        TextDumper dumper = new TextDumper(sw);
-//        dumper.dump(book);
-//
-//        sw.flush();
-//
-//        String dumpedText = sw.toString();
-//        assertThat(dumpedText, containsString(owner));
-//    }
+    @Test
+    void dumperDumpsAppointmentBookOwner() throws IOException, ParseException {
+        String description = "Eyes Check";
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+        var app = new Appointment("Jim", description, begin_date, end_date);
+        AppointmentBook book = new AppointmentBook("Jim");
+        book.addAppointment(app);
+
+        StringWriter sw = new StringWriter();
+        TextDumper dumper = new TextDumper(sw);
+        dumper.dump(book);
+
+        sw.flush();
+
+        String dumpedText = sw.toString();
+        assertThat(dumpedText, containsString("Jim"));
+    }
 }

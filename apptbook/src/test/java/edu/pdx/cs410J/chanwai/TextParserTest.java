193c0d6a78fa2f351d1227ee29803e4ab5b794f6
WaiHChan <chanwai@pdx.edu>
2021-07-20 22:28:57 -0700

    adding unit test for TextParserTest

--- apptbook/src/test/java/edu/pdx/cs410J/chanwai/TextParserTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/chanwai/TextParserTest.java
@@ -49,6 +49,35 @@ public class TextParserTest {
      * @throws ParserException Throw an Parser exception if the appointment book cannot be read
      */
     @Test
+    void appointmentBookOwnerCanAddQ() throws IOException, ParserException, ParseException {
+        String name = "\"Jim Chan\"";
+        String description = "Eyes";
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+        Date begin_date = df.parse("11/12/2019 " + "10:00 " + "am");
+        Date end_date = df.parse("11/12/2019 " + "10:30 " + "am");
+        var app = new Appointment(name, description, begin_date, end_date);
+
+        AppointmentBook book = new AppointmentBook(name);
+        book.addAppointment(app);
+
+        StringWriter sw = new StringWriter();
+        TextDumper dumper = new TextDumper(sw);
+        dumper.dump(book);
+
+        TextParser parser = new TextParser(new StringReader(sw.toString()));
+        book = parser.parse();
+
+        assertThat(book.getOwnerName(), equalTo(name));
+    }
+
+    /**
+     * Tests that invoking the dump() and parse() method from TestDump class and TestParser class
+     * Check if an appointment book is created and saved in StringWriter and
+     * Check if an appointment book can be parsed after the dump
+     * @throws IOException Throw an IO exception if the appointment cannot be dumped to a file
+     * @throws ParserException Throw an Parser exception if the appointment book cannot be read
+     */
+    @Test
     void appointmentBookParsed() throws IOException, ParserException, ParseException {
         String owner = "Jim";
         String description = "\"Eyes Check\"";
